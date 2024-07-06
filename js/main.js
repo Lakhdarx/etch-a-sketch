8594d16e67a01f27bd4a240f28090c1156c4920c
Lakhdar <Lakhdarbhl76@gmail.com>
2024-07-06 12:51:32 +0100

    Add hover effect to cells, black color only

--- js/main.js
+++ js/main.js
@@ -1,4 +1,15 @@
 const container = document.querySelector('.container');
+const button = document.querySelector('button');
+
+function hoverEffect() {
+    const cells = document.querySelectorAll('.cell');
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseenter', () => {
+            cell.style.backgroundColor = 'black';
+        });
+    });
+}
+
 
 for (let i = 0; i < 16; i++) {
     const row = document.createElement('div');
@@ -10,11 +21,32 @@ for (let i = 0; i < 16; i++) {
         row.appendChild(cell);
     }
 }
+hoverEffect();
 
-const cells = document.querySelectorAll('.cell');
-cells.forEach((cell) => {
-    cell.addEventListener('mouseenter', () => {
-        cell.style.backgroundColor = 'black';
-    });
+button.addEventListener('click', () => {
+    dimension = Number(prompt('Enter the dimensions of the grid (Less than 100X100)'));
+    if (isNaN(dimension) || dimension > 100) {
+        alert('Invalid Input');
+        return;
+    }
+
+    while (container.lastElementChild) {
+        container.removeChild(container.lastElementChild);
+    }
+
+    for (let i = 0; i < dimension; i++) {
+        const row = document.createElement('div');
+        row.setAttribute('class', 'row');
+        container.appendChild(row);
+        for (let j = 0; j < dimension; j++) {
+            const cell = document.createElement('div');
+            cell.setAttribute('class','cell');
+            row.appendChild(cell);
+        }
+    }
+
+    hoverEffect();
+
 });
+
 
